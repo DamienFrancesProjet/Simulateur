66c7762bbe98826a51f9c79a7d502a7fe3fe7b02
BetIzy <betizy.contact@gmail.com>
2016-04-15 01:36:53 +0200

    Création du Bundle Offer et suppression du Bundle par défault

--- src/FD/OfferBundle/FDOfferBundle.php
+++ src/FD/OfferBundle/FDOfferBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace FD\OfferBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class FDOfferBundle extends Bundle
 {
 }
